b6004808c996c3065e7554590d35ac458913d479
Evgeny Khabarov <ekhabarov@dev.ms>
2015-01-30 20:53:47 +0300

    Service info

---
+++ Server/GetServiceInfo.sql
@@ -0,0 +1,3 @@
+SELECT servicename, process_id, startup_type_desc, status_desc, 
+last_startup_time, service_account, is_clustered, cluster_nodename, [filename]
+FROM sys.dm_server_services WITH (NOLOCK) OPTION (RECOMPILE);
